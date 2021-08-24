a72d97e64f8d708a92f8a65e76660610543d91d8
yisu-kim <yisu.kim.dev@outlook.com>
2021-08-25 00:08:16 +0900

    Feat: #7 Add drag & drop

--- src/Pages/Delete/Delete.tsx
+++ src/Pages/Delete/Delete.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback, DragEvent, useRef } from 'react';
 import getDataFromLocalStorage from 'Utils/GetDataFromLocalStorage';
 import saveDataToLocalStorage from 'Utils/SaveDataToLocalStorage';
+import './Delete.css';
 
 export type Itodo = {
   id: number;
@@ -17,6 +18,9 @@ const initialTodos: Itodo[] = [];
 
 export default function Delete() {
   const [todoItems, setTodoItems] = useState(initialTodos);
+  const [dragging, setDragging] = useState(false);
+  const dragItem = useRef<number>();
+  const dragNode = useRef<EventTarget>();
 
   useEffect((): void => {
     fetch('/Data/Data.json')
@@ -39,15 +43,79 @@ export default function Delete() {
     saveData();
   }, [saveData]);
 
+  const findTodoItems = (id: number): number =>
+    todoItems.findIndex((todoItem) => todoItem.id === id);
+
+  const handleTodoItems = (targetId: number, currentId: number) => {
+    setTodoItems((prevTodoItems) => {
+      const targetIndex = findTodoItems(targetId);
+      const currentIndex = findTodoItems(currentId);
+
+      const newTodoItems = [...prevTodoItems];
+      newTodoItems.splice(
+        targetIndex,
+        0,
+        newTodoItems.splice(currentIndex, 1)[0]
+      );
+
+      return newTodoItems;
+    });
+  };
+
+  /* ---- Drag & Drop --- */
+  const handleDragStart = (e: DragEvent<HTMLDivElement>, id: number) => {
+    dragItem.current = id;
+    dragNode.current = e.target;
+
+    setTimeout(() => {
+      setDragging(true);
+    }, 0);
+  };
+
+  const handleDragEnd = () => {
+    dragItem.current = undefined;
+    dragNode.current = undefined;
+    setDragging(false);
+  };
+
+  const handleDragEnter = (e: DragEvent<HTMLDivElement>, targetId: number) => {
+    if (!dragging) {
+      return;
+    }
+    const currentId = dragItem.current!;
+    if (e.target !== dragNode.current) {
+      handleTodoItems(targetId, currentId);
+    }
+  };
+
+  const getDragItemStyle = (id: number): string | undefined => {
+    if (dragging) {
+      const currentId = dragItem.current!;
+      if (currentId === id) {
+        return 'drag-item';
+      }
+    }
+    return undefined;
+  };
+
   return (
-    <div>
+    <ul style={{ padding: '40px' }}>
       {todoItems.map(({ id, taskName }) => (
-        <div key={id}>
-          <div>{id}</div>
-          <div>{taskName}</div>
-          <input type="button" onClick={() => handleDeleteClick(id)} />
+        <div
+          key={id}
+          draggable
+          onDragStart={(e) => handleDragStart(e, id)}
+          onDragEnd={handleDragEnd}
+          onDragEnter={(e) => handleDragEnter(e, id)}
+          className={getDragItemStyle(id)}
+        >
+          <li className="todo-item">
+            <div>{id}</div>
+            <div>{taskName}</div>
+            <input type="button" onClick={() => handleDeleteClick(id)} />
+          </li>
         </div>
       ))}
-    </div>
+    </ul>
   );
 }
