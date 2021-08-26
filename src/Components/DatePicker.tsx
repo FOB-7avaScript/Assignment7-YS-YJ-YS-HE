ddb784e05dbe8dbb895b5457410a028fd43e6e35
yoonhe <yhehe228@gmail.com>
2021-08-26 10:10:24 +0900

    Design : #13 등록폼 UI 구현

--- src/Components/DatePicker.tsx
+++ src/Components/DatePicker.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { DateRange, OnDateRangeChangeProps, Range } from 'react-date-range';
 import { ko } from 'date-fns/locale';
+import styled from '@emotion/styled';
 
 import getDefaultDueDateRange from 'Utils/RangePicker';
 
@@ -9,10 +10,17 @@ import 'react-date-range/dist/theme/default.css';
 
 interface DatePickerProps {
   dueDateRange: Date[] | null;
+  className?: string;
   onChange: ({ value }: { value: Date[] }) => void;
+  onCloseClick: () => void;
 }
 
-const DatePicker: React.FC<DatePickerProps> = ({ dueDateRange, onChange }) => {
+const DatePicker: React.FC<DatePickerProps> = ({
+  dueDateRange,
+  className,
+  onChange,
+  onCloseClick
+}) => {
   const [dateRange, setDateRange] = useState<Range[]>([
     getDefaultDueDateRange({ dueDateRange })
   ]);
@@ -28,15 +36,37 @@ const DatePicker: React.FC<DatePickerProps> = ({ dueDateRange, onChange }) => {
   };
 
   return (
-    <DateRange
-      ranges={dateRange}
-      onChange={handleDateRangeChange}
-      moveRangeOnFirstSelection={false}
-      locale={ko}
-      minDate={new Date()}
-      dateDisplayFormat="yyyy / MM / dd"
-    />
+    <Wrap className={className}>
+      <DateRange
+        ranges={dateRange}
+        onChange={handleDateRangeChange}
+        moveRangeOnFirstSelection={false}
+        locale={ko}
+        minDate={new Date()}
+        dateDisplayFormat="yyyy / MM / dd"
+      />
+      <Button type="button" onClick={onCloseClick}>
+        닫기
+      </Button>
+    </Wrap>
   );
 };
+
+const Wrap = styled.div`
+  display: inline-block;
+`;
+
+const Button = styled.button`
+  display: block;
+  padding: 15px 0;
+  width: 100%;
+  color: #aeb9bf;
+  background: #f8f8f8;
+  cursor: pointer;
+  &:hover {
+    background: #0099fd;
+    color: #fff;
+  }
+`;
 
 export default DatePicker;
--- src/Components/TodoForm.tsx
+++ src/Components/TodoForm.tsx
@@ -1,6 +1,12 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { FaCalendarAlt } from 'react-icons/fa';
+
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
 import useTodo from 'hooks/useTodo';
+import useRangePickerVisible from 'hooks/useRangePickerVisible';
+
 import dateFormat from 'Utils/Date';
 import DatePicker from './DatePicker';
 
@@ -8,30 +14,93 @@ const TodoForm: FC = () => {
   const { todo, handleInputChange, handleDateRangeChange } = useTodo();
   const { taskName, dueDateRange } = todo;
 
-  const [rangePickerOpen, setRangePickerOpen] = useState(false);
-
-  const handleRangePickerVisibleToggle = () => {
-    setRangePickerOpen((prev: boolean) => !prev);
-  };
+  const {
+    rangePickerOpen,
+    handleRangePickerVisibleToggle,
+    handleCloseButtonClick
+  } = useRangePickerVisible();
 
   return (
-    <form>
+    <FormWrap>
       <h2>{dateFormat({ targetDate: new Date() })}</h2>
-      <p>
-        <input name="taskName" value={taskName} onChange={handleInputChange} />
-        <button type="button" onClick={handleRangePickerVisibleToggle}>
-          <FaCalendarAlt />
-        </button>
-      </p>
-      {rangePickerOpen && (
-        <DatePicker
-          dueDateRange={dueDateRange}
-          onChange={handleDateRangeChange}
-        />
-      )}
-      <button type="submit">추가</button>
-    </form>
+      <form>
+        <InputBox>
+          <input
+            name="taskName"
+            value={taskName}
+            onChange={handleInputChange}
+          />
+          <IconButton
+            type="button"
+            onClick={handleRangePickerVisibleToggle}
+            active={rangePickerOpen}
+          >
+            <FaCalendarAlt />
+          </IconButton>
+        </InputBox>
+        {rangePickerOpen && (
+          <CustomDatePicker
+            dueDateRange={dueDateRange}
+            onChange={handleDateRangeChange}
+            onCloseClick={handleCloseButtonClick}
+          />
+        )}
+        <Button type="submit">추가</Button>
+      </form>
+    </FormWrap>
   );
 };
+
+const FormWrap = styled.div`
+  display: inline-block;
+  h2 {
+    margin-bottom: 10px;
+  }
+  form {
+    display: flex;
+    position: relative;
+  }
+`;
+
+const InputBox = styled.p`
+  input {
+    padding: 10px;
+    width: 300px;
+    border: 1px solid #dcdcdc;
+    outline: none;
+  }
+`;
+
+const CustomDatePicker = styled(DatePicker)`
+  position: absolute;
+  top: 45px;
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: 0px 4px 15px rgb(0 0 0 / 20%);
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  margin-left: 5px;
+  height: 37px;
+  color: #999;
+  border: 1px solid #dcdcdc;
+  vertical-align: middle;
+  cursor: pointer;
+  &:hover {
+    color: #0099fd;
+    border-color: #0099fd;
+  }
+`;
+
+const IconButton = styled(Button)`
+  width: 37px;
+  ${({ active }: { active?: boolean }) =>
+    active &&
+    css`
+      color: #0099fd;
+      border-color: #0099fd;
+    `}
+`;
 
 export default TodoForm;
